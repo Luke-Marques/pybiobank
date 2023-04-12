6a6a43f0965bdcab492841b8e4033b06cfd91448
Luke Marques <luke.o.marques@gmail.com>
2023-04-12 11:39:49 +0100

    fixed duplicate fields in read_ukb_field_finder

--- pybiobank/phenotype.py
+++ pybiobank/phenotype.py
@@ -41,6 +41,7 @@ def read_ukb_field_finder(
     field_finder_dfs = [pl.read_csv(f, sep='\t', has_header=True).with_columns([pl.lit(str(f.stem).split('_')[0]).alias('basket'), pl.lit(str(str(f.absolute()))).alias('path')]) for f in field_finder_files]
     field_finder = pl.concat(field_finder_dfs) \
         .unique() \
+        .unique(subset='field') \
         .filter(pl.col('field') != 'eid')
     
     return field_finder
@@ -49,8 +50,7 @@ def read_ukb_field_finder(
 def read_ukb_phenotype_fields(
     ukb_project_dir: str or Path or PosixPath or WindowsPath,
     ukb_fields: List[str],
-    ukb_project_phenotype_subdir_name: str or Path or PosixPath or WindowsPath = Path('phenotypes'),
-    exact: bool = True
+    ukb_project_phenotype_subdir_name: str or Path or PosixPath or WindowsPath = Path('phenotypes')
 ) -> pl.DataFrame:
     """_summary_
 
@@ -76,16 +76,13 @@ def read_ukb_phenotype_fields(
         pass
     
     # get field_finder data frame and filter to required fields
-    if exact:
-        field_finder = read_ukb_field_finder(
-            ukb_project_dir=ukb_project_dir, 
-            ukb_project_phenotype_subdir_name=ukb_project_phenotype_subdir_name
-        ) \
-            .filter(pl.col('field').is_in(ukb_fields))
-    else:
-        pass  # TO-DO: add in messy filtering of ukb_fields
+    field_finder = read_ukb_field_finder(
+        ukb_project_dir=ukb_project_dir, 
+        ukb_project_phenotype_subdir_name=ukb_project_phenotype_subdir_name
+    ) \
+        .filter(pl.col('field').is_in(ukb_fields))
     
-    # create dictionary to map UKB to Polars dtypes
+    # create dictionary to map UKB dtypes to Polars dtypes
     dtype_dict = {
         'Sequence': pl.Int64,
         'Integer': pl.Int64,
@@ -105,10 +102,10 @@ def read_ukb_phenotype_fields(
     baskets = field_finder['basket'].unique().to_list()
     pheno_dfs = []
     for basket in baskets:
-        fields = ['eid'] + field_finder.filter(pl.col('basket') == basket)['field'].unique().to_list()
+        fields = ['eid'] + field_finder.filter(pl.col('basket') == basket)['field'].to_list()
         dtypes = [pl.Int64] + list(map(lambda x: dtype_dict.get(x, pl.Unknown), field_finder.filter(pl.col('basket') == basket)['ukb_type'].to_list()))
         pheno_file = ukb_project_dir / ukb_project_phenotype_subdir_name / Path(f'{basket}.csv')
         pheno_dfs.append(pl.read_csv(pheno_file, columns=fields, has_header=True, dtypes=dtypes))
     pheno = reduce(lambda left_df, right_df: left_df.join(other=right_df, on='eid', how='outer'), pheno_dfs)
-    
+
     return pheno
