0b44ee616affe960c7ffb8f937c39c17c560be3e
Luke Marques <luke.o.marques@gmail.com>
2023-04-25 12:12:55 +0100

    fixed bugs in genotype module's unit tests

--- tests/test_genotype.py
+++ tests/test_genotype.py
@@ -1,8 +1,9 @@
 import unittest
+import gzip
 import polars as pl
 from polars.testing import assert_frame_equal
 from pathlib import Path
-from pybiobank.genotype import read_fam_file
+from pybiobank.genotype import read_fam_file, read_sample_quality_control_file
 
 
 class DFTests(unittest.TestCase):
@@ -15,16 +16,17 @@ class DFTests(unittest.TestCase):
         UK Biobank project 23203 on the King's HPC, CREATE.
         """
         TEST_DATA_DIR = Path('data')
-        test_fam_file = TEST_DATA_DIR / 'pybiobank.test.ukb23203.fam'
+        test_fam_file = TEST_DATA_DIR / 'pybiobank.test.ukb23203.fam.gz'
         ukb_project_dir = Path(
             '/scratch',
             'prj',
             'premandm',
             'ukb23203_rga'
         )
-        test_fam = pl.read_csv(test_fam_file, separator='\t')
+        test_fam = pl.read_csv(gzip.open(test_fam_file, 'rb').read(), separator='\t')
         fam = read_fam_file(ukb_project_dir=ukb_project_dir)
         assert_frame_equal(test_fam, fam)
+        print()
         
     def test_read_sample_quality_control_file(self):
         """
@@ -33,13 +35,13 @@ class DFTests(unittest.TestCase):
         from the UK Biobank project 23203 on the King's HPC, CREATE.
         """
         TEST_DATA_DIR = Path('data')
-        test_sqc_file = TEST_DATA_DIR / 'pybiobank.test.ukb23203.sqc'
+        test_sqc_file = TEST_DATA_DIR / 'pybiobank.test.ukb23203.sqc.gz'
         ukb_project_dir = Path(
             '/scratch',
             'prj',
             'premandm',
             'ukb23203_rga'
         )
-        test_sqc = pl.read_csv(test_sqc_file, separator='\t')
-        sqc = read_fam_file(ukb_project_dir=ukb_project_dir)
+        test_sqc = pl.read_csv(gzip.open(test_sqc_file, 'rb').read(), separator='\t')
+        sqc = read_sample_quality_control_file(ukb_project_dir=ukb_project_dir)
         assert_frame_equal(test_sqc, sqc)
